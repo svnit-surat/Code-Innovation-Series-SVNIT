f3905647c32c76de0998a194a5d92419a2263351
Astel <astelthottankara@gmail.com>
2020-12-24 22:45:52 +0530

    fix socket connection issues

--- Mind Install/Doubt_Solver/index.js
+++ Mind Install/Doubt_Solver/index.js
@@ -6,8 +6,11 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const port = 3000;
+let username = 'New User';
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
@@ -15,14 +18,22 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.post('/', (req, res) => {
+    username = req.body.firstname;
+    res.redirect('/home');
+})
+
 app.get('/home', (req, res) => {
-    res.render('query');
+    res.render('query', { username });
 });
 
 io.on('connection', (socket) => {
-    console.log('A new user connected');
+    console.log(`${username} connected`);
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log(`${username} disconnected`);
     });
 });
 
--- Mind Install/Doubt_Solver/public/socket.js
+++ Mind Install/Doubt_Solver/public/socket.js
@@ -1,1 +1,9 @@
-var socket = io();
+$(function () {
+    var socket = io();
+    $('form').submit(function(e) {
+        e.preventDefault();
+        socket.emit('chat message', $('#question').val());
+        $('#question').val('');
+        return false;
+    });
+});
