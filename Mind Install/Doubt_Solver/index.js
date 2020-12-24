ba8ea670512df4daee91763e1ea8846fd798ce8e
Astel <astelthottankara@gmail.com>
2020-12-24 19:21:21 +0530

    add socket connection

--- Mind Install/Doubt_Solver/index.js
+++ Mind Install/Doubt_Solver/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
-app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
-})
+io.on('connection', (socket) => {
+    console.log('A new user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+    console.log(`Server listening on port ${port}`);
+});
