7891503a00d912fd478546ad6cb3d16d5a7463d8
Astel <astelthottankara@gmail.com>
2020-12-25 00:06:48 +0530

    feat: add conversation threading

--- Mind Install/Doubt_Solver/public/socket.js
+++ Mind Install/Doubt_Solver/public/socket.js
@@ -1,9 +1,24 @@
-$(function () {
-    var socket = io();
-    $('form').submit(function(e) {
-        e.preventDefault();
-        socket.emit('chat message', $('#question').val());
-        $('#question').val('');
-        return false;
+$(document).ready(function() {
+    $(function () {
+        const socket = io();
+        $('form').submit(function(e) {
+            e.preventDefault(); // prevents page reloading
+            console.log($('#query').val());
+            socket.emit('new thread', $('#query').val());
+            $('#query').val('');
+            return false;
+        });
+        socket.on('new thread', function(msg){
+            let newMsg = '<div class="main-thread card card-body bg-light blockquote">' +
+                        '<p class="mb-0">thread1 '+msg+'</p>' +
+                        '<div class="blockquote-footer">student name, time</div></div>'
+            $('#physics').append(newMsg);
+        });
+        socket.on('reply thread', function(msg){
+            let newMsg = '<div class="sub-thread card card-body bg-light blockquote">' +                      
+                        '<p class="mb-0">'+msg+'</p>' +
+                        '<div class="blockquote-footer">student name, time</div></div>'
+            $('#physics_1').append(newMsg);
+        });
     });
-});
+})
