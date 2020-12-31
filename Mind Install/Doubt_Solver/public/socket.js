4eb0777edf32eb916f9d680e6799fa00f94abf91
dhrutik <star.dhrutikpatel@gmail.com>
2020-12-31 05:52:13 +0530

    inline form added

--- Mind Install/Doubt_Solver/public/socket.js
+++ Mind Install/Doubt_Solver/public/socket.js
@@ -16,10 +16,12 @@ $(document).ready(function() {
             let newMsg = '<div class="main-thread card card-body bg-light blockquote">' +
                             '<p class="mb-0">'+msg+'</p>' +
                         '<div class="blockquote-footer">' + username['username'] + ', '+time+'</div>' +
-                        '<div class="text-right"><button id="thread_'+physics_id+'" type="submit" class="btn btn-outline-primary" >'+
-                        '<a data-toggle="modal" onclick="question()">Reply</a>'+
-                        '</button>&nbsp;&nbsp;&nbsp;&nbsp; 30 &uarr; upvotes</div></div>'
-            $('#physics').append(newMsg);
+                        '<div class="text-right">'+
+                        '&nbsp;&nbsp;&nbsp;&nbsp; 30 &uarr; upvotes</div>'+
+                        '<form class="form" method="POST" action="/home"><div class="form-row"><div class="col-12 col-lg-10 col-sm-10"><textarea type="text" class="form-control mb-2 mr-sm-2" id="query" name="query" placeholder="Enter your question"  row="200"></textarea></div><div class="col col-lg-2 col-sm"><button type="submit" class="btn btn-primary mb-2">Send</button></div></div></form>'
+                        +'</div>'
+                        
+            $('#physicsMainThread').append(newMsg);
             physics_id++;
         });
         socket.on('reply thread', function(msg){
